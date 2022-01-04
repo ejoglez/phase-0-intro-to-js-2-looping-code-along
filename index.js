fc0e1b202b0ae671cf124e0bec5e960930ad2872
Eric Gonzalez <ejoglez@gmail.com>
2022-01-04 04:58:16 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const people = ['Guadalupe', 'Ollie', 'Aki'];
+
+function writeCards(people,emotion){
+
+  const displayArray = [];
+  for(let i = 0; i < people.length; i++) { 
+    displayArray.push(`Thank you, ${people[i]}, for the wonderful ${emotion} gift!`);
+    } 
+        return displayArray;
+  }
+
+writeCards(['Guadalupe', 'Ollie', 'Aki'], "surprise");
+
+function countDown(){
+    let count = 10;
+    while (count >= 0) {
+        console.log(count--);
+    }
+}
+countDown(10);
